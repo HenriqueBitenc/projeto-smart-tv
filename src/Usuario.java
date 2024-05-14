f173bebb1fc6ccd71bc35925f6335bc1c61fc3fb
HenriqueBitenc <henriqueb.bitencourte@gmail.com>
2024-05-14 19:52:34 -0300

    Projeto SmartTV finalizado

---
+++ src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(15);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+
+    }
+}
